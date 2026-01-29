a8eaed1dc9e39f569ba81ee34a9c88af8687ef2d
Angela Ruecker <angelrck@gmail.com>
2026-01-29 11:36:02 +0100

    version tests corrigés

--- frontend/cypress/e2e/UI/cartAdd.cy.js
+++ frontend/cypress/e2e/UI/cartAdd.cy.js
@@ -1,64 +1,77 @@
 describe("Check stock decrease after adding product to cart", () => {
-    const apiUrl = Cypress.env('apiUrl')
     const productId = 10
 
     const login = () => {
         cy.clearLocalStorage()
         cy.clearCookies()
-        cy.visit('/#/login')
-        cy.get("[data-cy='login-input-username']").type('test2@test.fr')
-        cy.get("[data-cy='login-input-password']").type('testtest')
+
+        cy.intercept("POST", "**/login").as("login")
+
+        cy.visit("/#/login")
+        cy.get("[data-cy='login-input-username']").type("test2@test.fr")
+        cy.get("[data-cy='login-input-password']").type("testtest")
         cy.get("[data-cy='login-submit']").click()
-        cy.url().should('not.include', '/login')
+
+        cy.wait("@login")
+        cy.url().should("not.include", "/login")
     }
 
     const emptyCart = () => {
-        cy.visit('/#/cart')
-        cy.get('body').then(($body) => {
+        cy.intercept("DELETE", "**/orders/*/delete").as("deleteItem")
+
+        cy.visit("/#/cart")
+
+        cy.get("body").then(($body) => {
             if ($body.find("[data-cy='cart-line']").length > 0) {
-                cy.get("[data-cy='cart-line']").each(() => {
+                const deleteButtons = $body.find("[data-cy='cart-line-delete']")
+                const count = deleteButtons.length
+
+                for (let i = 0; i < count; i++) {
                     cy.get("[data-cy='cart-line-delete']").first().click()
-                })
-                cy.get("[data-cy='cart-line']").should('not.exist')
+                    cy.wait("@deleteItem")
+                }
+
+                cy.get("[data-cy='cart-line']").should("not.exist")
             }
         })
     }
 
     it("should decrease stock after adding product to cart", () => {
+        cy.intercept("GET", `**/products/${productId}`).as("getProduct")
+        cy.intercept("PUT", "**/orders/add").as("addToCart")
+
         login()
         emptyCart()
 
-        cy.intercept('GET', `**/products/${productId}`).as('getProduct')
-        cy.intercept('PUT', '**/orders/add').as('addToCart')
-
         cy.visit(`/#/products/${productId}`)
-        cy.wait('@getProduct')
-        cy.get("[data-cy='detail-product-name']").should('be.visible')
+        cy.wait("@getProduct")
+        cy.get("[data-cy='detail-product-name']").should("be.visible")
 
         cy.get("[data-cy='detail-product-stock']")
-            .should('be.visible')
-            .invoke('text')
-            .should('match', /\d+/)
+            .should("be.visible")
+            .invoke("text")
+            .should("match", /\d+/)
             .then((text) => {
                 const initialStock = parseInt(text.match(/\d+/)[0])
 
                 cy.get("[data-cy='detail-product-add']")
-                    .should('be.visible')
+                    .should("be.visible")
+                    .should("not.be.disabled")
                     .click()
 
-                cy.wait('@addToCart')
+                cy.wait("@addToCart")
 
                 cy.visit(`/#/products/${productId}`)
-                cy.wait('@getProduct')
-                cy.get("[data-cy='detail-product-name']").should('be.visible')
+                cy.wait("@getProduct")
+                cy.get("[data-cy='detail-product-name']").should("be.visible")
 
                 cy.get("[data-cy='detail-product-stock']")
-                    .should('be.visible')
-                    .invoke('text')
-                    .should('match', /\d+/)
+                    .should("be.visible")
+                    .invoke("text")
+                    .should("match", /\d+/)
                     .then((newText) => {
                         const newStock = parseInt(newText.match(/\d+/)[0])
-                        expect(newStock).to.equal(initialStock - 1)
+                        expect(newStock).to.eq(initialStock - 1)
                     })
             })
     })
--- frontend/cypress/e2e/UI/reviews.cy.js
+++ frontend/cypress/e2e/UI/reviews.cy.js
@@ -1,39 +1,80 @@
 describe("XSS protection on reviews", () => {
+
+    const apiUrl = Cypress.env("apiUrl")
+
+    const login = () => {
+        cy.clearLocalStorage()
+        cy.clearCookies()
+
+        cy.intercept("POST", "**/login").as("login")
+
+        cy.visit("/#/login")
+        cy.get("[data-cy='login-input-username']").type("test2@test.fr")
+        cy.get("[data-cy='login-input-password']").type("testtest")
+        cy.get("[data-cy='login-submit']").click()
+
+        cy.wait("@login")
+        cy.url().should("not.include", "/login")
+    }
+
     it("should not execute XSS script in review comment", () => {
-        cy.visit('/#/login')
-        cy.get("[data-cy='login-input-username']").type('test2@test.fr')
-        cy.get("[data-cy='login-input-password']").type('testtest')
-        cy.get("[data-cy='login-submit']").click()
-        cy.url().should('not.include', '/login')
-        cy.get("[data-cy='nav-link-reviews']").should('be.visible').click()
-        cy.get("[data-cy='review-input-rating-images']")
-            .find('img')
+        login()
+
+        cy.intercept("POST", "**/reviews").as("postReview")
+        cy.intercept("GET", "**/reviews").as("getReviews")
+
+        cy.on("window:alert", () => {
+            throw new Error("XSS detected: alert was triggered")
+        })
+
+        cy.visit("/#/reviews")
+
+        cy.wait("@getReviews")
+
+        cy.get("[data-cy='review-input-rating-images'] img", { timeout: 10000 })
             .eq(4)
             .click()
-        cy.get("[data-cy='review-input-title']").type('Parfum de vacances')
-        cy.get("[data-cy='review-input-comment']").type('<script>alert("faille XSS");</script>')
-        cy.on('window:alert', () => {
-            throw new Error('XSS detected: alert was triggered')
-        })
+
+        cy.get("[data-cy='review-input-title']").type("Parfum de vacances")
+        cy.get("[data-cy='review-input-comment']")
+            .type('<script>alert("faille XSS");</script>')
+
         cy.get("[data-cy='review-submit']").click()
+
+        cy.wait("@postReview")
     })
+
     it("should not execute XSS script in review title", () => {
-        cy.visit('/#/login')
-        cy.get("[data-cy='login-input-username']").type('test2@test.fr')
-        cy.get("[data-cy='login-input-password']").type('testtest')
-        cy.get("[data-cy='login-submit']").click()
-        cy.url().should('not.include', '/login')
-        cy.get("[data-cy='nav-link-reviews']").should('be.visible').click()
-        cy.get("[data-cy='review-input-rating-images']")
-            .find('img')
+        login()
+
+        cy.intercept("POST", "**/reviews").as("postReview")
+        cy.intercept("GET", "**/reviews").as("getReviews")
+
+        cy.on("window:alert", () => {
+            throw new Error("XSS detected: alert was triggered")
+        })
+
+        cy.visit("/#/reviews")
+
+        cy.wait("@getReviews")
+
+        cy.get("[data-cy='review-input-rating-images'] img", { timeout: 10000 })
             .eq(4)
             .click()
-        cy.get("[data-cy='review-input-title']").type('<script>alert("faille XSS");</script>')
-        cy.get("[data-cy='review-input-comment']").type('Bonne odeur de pluie')
-        cy.on('window:alert', () => {
-            throw new Error('XSS detected: alert was triggered')
-        })
+
+        cy.get("[data-cy='review-input-title']")
+            .type('<script>alert("faille XSS");</script>')
+        cy.get("[data-cy='review-input-comment']").type("Bonne odeur de pluie")
+
         cy.get("[data-cy='review-submit']").click()
+
+        cy.wait("@postReview")
+
+        cy.visit("/#/reviews")
+
+        cy.wait("@getReviews")
+
+        cy.get("[data-cy='review-title']")
+            .should("not.contain.text", "script")
     })
 })
-
